4e95da978cc3251e9fdf8ad033e84f4b860f6abb
Patrick Oser <patrick.oser148@gmail.com>
2025-12-15 16:06:05 -0500

    Add Header to App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './pages/Home.tsx'
+import Header from './components/Header.tsx'
 
 function App() {
 
 
   return (
     <>
+      <Header />
       <Home />
     </>
   )
