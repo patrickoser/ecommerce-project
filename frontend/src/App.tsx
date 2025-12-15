c39415bec32a3e0df99e10b476ba4eb201957009
Patrick Oser <patrick.oser148@gmail.com>
2025-12-15 16:09:35 -0500

    Add Footer to App.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Home from './pages/Home.tsx'
 import Header from './components/Header.tsx'
+import Footer from './components/Footer.tsx'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   )
 }
