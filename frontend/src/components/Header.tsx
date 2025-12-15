823f5f7d9385cfe4585e63659d61e4eed5f41b95
Patrick Oser <patrick.oser148@gmail.com>
2025-12-15 16:12:10 -0500

    Add ph elements.

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+
+
+const Header = () => {
+
+    return (
+        <div className="">
+            <h3>Home</h3>
+            <h3>Search Bar</h3>
+            <h3>Hamburger</h3>
+        </div>
+    )
+}
+
+export default Header
