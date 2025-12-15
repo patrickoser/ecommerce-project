631dcfdcfb123ed2cba9941ff58fc669964e10cb
Patrick Oser <patrick.oser148@gmail.com>
2025-12-15 16:02:24 -0500

    Add Header component file.

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -2,7 +2,7 @@
 
 const ProductCard = () => {
     return (
-        <div>
+        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <h1>ProductCard</h1>
         </div>
     )
