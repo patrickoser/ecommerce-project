ccdb3f5d7c1f65bd431fe7144081556c4c71ac43
Patrick Oser <patrick.oser148@gmail.com>
2025-12-16 17:41:14 -0500

    Misc.

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -2,8 +2,9 @@
 
 const ProductCard = () => {
     return (
-        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <h1>ProductCard</h1>
+        <div className="">
+            <h1 >ProductCard</h1>
+            
         </div>
     )
 }
