ab9540bace50addbcdda485fadbc942cc602df9b
Patrick Oser <patrick.oser148@gmail.com>
2026-05-13 17:39:14 -0400

    Small change.

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,6 +1,10 @@
-
+import { Product } from "../types/Product";
 
 const ProductCard = () => {
+    interface ProductCardProps {
+        product: Product;
+        
+    }
     return (
         <div className="">
             <h1 >ProductCard</h1>
---
+++ frontend/src/types/Product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+    inStock: boolean;
+}
