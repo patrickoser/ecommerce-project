77d77b69745d35a770272b1bd2b21504fe217ba3
Patrick Oser <patrick.oser148@gmail.com>
2026-05-20 15:57:17 -0400

    Misc.

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -12,8 +12,8 @@ const ProductCard = ({ product }: ProductCardProps) => {
             <img src={product.imageUrl} alt={product.name} />
             <h1 >{product.name}</h1>
             <p>${product.price}</p>
-            <p>{product.inStock}</p>
-            <p>{product.description ? 'In Stock' : 'Not In Stock'}</p>
+            <p>{product.inStock ? 'In Stock' : 'Not In Stock'}</p>
+            <p>{product.description}</p>
         </div>
     )
 }
