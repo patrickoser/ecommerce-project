5d67dcf9ffeceff0664b1b3702d096583da26179
Patrick Oser <patrick.oser148@gmail.com>
2026-05-28 16:04:29 -0400

    Mock data.

--- frontend/src/data/products.ts
+++ frontend/src/data/products.ts
@@ -1,5 +1,28 @@
 import type { Product } from "../types/Product"
 
 export const products: Product[] = [
-    
+    {
+        id: "1234",
+        name: "book",
+        imageUrl: "https://placehold.co/300x300?text=Book",
+        price: 19.99,
+        inStock: true,
+        description: "This a book",
+    },
+    {   
+        id: "12345",
+        name: "laptop",
+        imageUrl: "https://placehold.co/300x300",
+        price: 999.99,
+        inStock: false,
+        description: "This a laptop",
+    },
+    {   
+        id: "12346",
+        name: "shoes",
+        imageUrl: "https://placehold.co/300x300",
+        price: 69.99,
+        inStock: true,
+        description: "This a shoe",
+    }
 ]
