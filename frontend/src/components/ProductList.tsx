887207fbc0305b782de04a7a89bd753467b8d729
Patrick Oser <patrick.oser148@gmail.com>
2026-05-31 18:40:04 -0400

    Create ProductList file.

---
+++ frontend/src/components/ProductList.tsx
@@ -0,0 +1,10 @@
+import type { Product } from "../types/Product";
+import { products } from "../data/products";
+
+const ProductList = () => {
+    return (
+
+    )
+}
+
+export default ProductList
