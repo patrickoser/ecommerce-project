f07345266339054e703442c735f7cb53fd866b02
Patrick Oser <patrick.oser148@gmail.com>
2026-06-04 14:25:59 -0400

    Fix return issue.

--- frontend/src/components/ProductList.tsx
+++ frontend/src/components/ProductList.tsx
@@ -1,9 +1,14 @@
 import type { Product } from "../types/Product";
 import { products } from "../data/products";
+import { ProductCard } from "../components/ProductCard"
 
 const ProductList = () => {
     return (
-
+        <div>
+            {products.map((product) => (
+                <ProductCard product={product} key={product.id} />
+            ))}
+        </div>
     )
 }
 
